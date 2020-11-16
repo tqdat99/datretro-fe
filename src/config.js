76970a82abd23439468c9e20b7bc980cc9eb5620
tqdat99 <tqdat9@gmail.com>
2020-11-16 21:26:12 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,13 @@
+import dotenv from "dotenv";
+
+// Set the NODE_ENV to 'development' by default
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+const envFound = dotenv.config();
+if (!envFound) {
+    throw new Error("Couldn't find .env file");
+}
+
 export const config = {
     "cookie_token": "cookie_token",
     "cookie_username": "cookie_username",
