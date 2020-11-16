6aead638ad117e82186ff3d8119afeff667541af
tqdat99 <tqdat9@gmail.com>
2020-11-17 00:58:41 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -8,5 +8,5 @@ if (!envFound) {
 export const config = {
     "cookie_token": "cookie_token",
     "cookie_username": "cookie_username",
-    "api_url": process.env.API_URL || 'http://localhost:5000/api'
+    "api_url": process.env.API_URL
 };
