3b125f2ca1124e09429a4619e635b6d999dcf96a
tqdat99 <tqdat9@gmail.com>
2020-11-16 21:35:22 +0700

    API_URL Update

--- src/config.js
+++ src/config.js
@@ -1,8 +1,5 @@
 import dotenv from "dotenv";
 
-// Set the NODE_ENV to 'development' by default
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
-
 const envFound = dotenv.config();
 if (!envFound) {
     throw new Error("Couldn't find .env file");
@@ -11,5 +8,5 @@ if (!envFound) {
 export const config = {
     "cookie_token": "cookie_token",
     "cookie_username": "cookie_username",
-    "api_url": process.env.API_URL || 'http://localhost:5000/api'
+    "api_url": process.env.API_URL || 'https://datretro-be.herokuapp.com/api'
 };
